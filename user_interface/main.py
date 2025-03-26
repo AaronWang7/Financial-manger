0c8c8f25b40abbe5a268f8c01de6d1b4f92dd650
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-03-26 10:30:58 -0600

    pls fix input future moi :)

--- user_interface/main.py
+++ user_interface/main.py
@@ -7,38 +7,42 @@
 ####
 
 from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
 
 def main():
     while True:
         print("----------Personal Financial Manager program----------")
         try:
-            name = inquirer.text(message="What's your name:").execute()
-            fav_lang = inquirer.select(
-                message="What's your favourite programming language:",
-                choices=["Go", "Python", "Rust", "JavaScript"],
+            intro = inquirer.text(message="Welcome ot your personal financial manager! What's your name?").execute()
+            per_fina_opt = inquirer.select(
+                message="What would you like to do?:",
+                choices= ["Go to the Savings Goal Tracker", "Data Manager", "Convert Currency", "Check Budgetting", "Exit"],
             ).execute()
-            confirm = inquirer.confirm(message="Confirm?").execute()
+            if intro:
+                print("hello! GOODBYE")
+                
             #main = int(input("""\n Welcome to your personal financial manager! What would you like to check and update?
                          #1. Goal Savings Tracker
                          #2. Data Manager
                          #3. Convert Currency
                          #4. Check Budget
                          #5. Exit\n"""))
-            if main == 1:
-                print("This is the '1' option, if it is working, then great! Don't forget to callback the actual functions here later!")
-                continue
-            elif main == 2:
-                print("This is the '2' option, if it is working, then great! Don't forget to callback the actual functions here later!")
-                continue
-            elif main == 3:
-                print("This is the '3' option, if it is working, then great! Don't forget to callback the actual functions here later!")
-                continue
-            elif main == 4:
-                print("This is the '4' option, if it is working, then great! Don't forget to callback the actual functions here later!")
-                continue
-            elif main == 5:
-                print("Thank you for using your personal program, goodbye!")
-                break
+            #if main == 1:
+                #print("This is the '1' option, if it is working, then great! Don't forget to callback the actual functions here later!")
+                #continue
+            #elif main == 2:
+                #print("This is the '2' option, if it is working, then great! Don't forget to callback the actual functions here later!")
+                #continue
+            #elif main == 3:
+                #print("This is the '3' option, if it is working, then great! Don't forget to callback the actual functions here later!")
+                #continue
+            #elif main == 4:
+                #print("This is the '4' option, if it is working, then great! Don't forget to callback the actual functions here later!")
+                #continue
+            #elif main == 5:
+                #print("Thank you for using your personal program, goodbye!")
+                #break
             
         except:
             ValueError
