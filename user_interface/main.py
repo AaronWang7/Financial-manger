50b849f4d145c4c150c86e5def3bcd205662c120
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-04-07 10:40:23 -0600

    ok

--- user_interface/main.py
+++ user_interface/main.py
@@ -22,25 +22,25 @@ def main(): #Main user interface
     while True: #While loop
         print(args[0]) #Fancy fancy stuff, allows for direct and specific things on the variable for what it displays.
         try:
-            intro = inquirer.text(message="Welcome ot your personal financial manager! What's your name?").execute()
-            print(f"Welcome {intro}.") #Repeats the username with a welcome message.
+            intro = inquirer.text(message="Welcome ot your personal financial manager! What was your name?").execute()
+            print(f"Welcome {intro}!") #Repeats the username with a welcome message.
             pers_finan_opt = inquirer.select( #Main prompt
                 message="What would you like to do?:", #Questions
                 choices= ["Go to the Savings Goal Tracker", "Data Manager", "Convert Currency", "Check Budgetting", "Exit"],
                 filter=lambda result: result.split()[0].lower() #Allows the user to be able to input their response correctly.
             ).execute()
             if pers_finan_opt == "go": #Different choices that go into the different functions, this one goes into the Savings/Goal Trackers.
-                pass
-                break
+                
+                continue
             elif pers_finan_opt == "data": #This one goes into the Data Manager.
-                pass
-                break
+                
+                continue
             elif pers_finan_opt == "convert": #This one goes into the Convert Currency.
-                pass
-                break
+                
+                continue
             elif pers_finan_opt == "check": #This one goes into the Check Budgetting.
-                pass
-                break
+                
+                continue
             elif pers_finan_opt == "exit": #As the name implies, exits and quits the program.
                 print("Goodbye, thank you for using your personal financial manager!")
                 break
@@ -56,7 +56,7 @@ main()
 ####
 ####
 ####
-#Finished program, remember add in the actual function names.
+#Finished program, remember to add in the actual function names.
 ####
 ####
 ####
