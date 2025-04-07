8c7b812b8697a96e37da7c6547e8f1675ea1adca
EthanBlanco15 <ethan.blanco@ucas-edu.net>
2025-04-07 10:30:55 -0600

    Last "main" commit

--- user_interface/main.py
+++ user_interface/main.py
@@ -16,11 +16,11 @@ from InquirerPy import inquirer
 from InquirerPy.base.control import Choice
 from InquirerPy.separator import Separator
 
-args = ["----------Personal Financial Manager program----------"]
+args = ["----------Personal Financial Manager program----------"] #Extra menu text to introduce the program.
 
 def main(): #Main user interface
-    while True:
-        print(args[0])
+    while True: #While loop
+        print(args[0]) #Fancy fancy stuff, allows for direct and specific things on the variable for what it displays.
         try:
             intro = inquirer.text(message="Welcome ot your personal financial manager! What's your name?").execute()
             print(f"Welcome {intro}.") #Repeats the username with a welcome message.
@@ -29,24 +29,24 @@ def main(): #Main user interface
                 choices= ["Go to the Savings Goal Tracker", "Data Manager", "Convert Currency", "Check Budgetting", "Exit"],
                 filter=lambda result: result.split()[0].lower() #Allows the user to be able to input their response correctly.
             ).execute()
-            if pers_finan_opt == "go": #Different choices that go into the different functions.
-                print("This is the '1' option, 'Savings Goal Tracker', if it is working, then great! Don't forget to callback the actual functions here later!")
+            if pers_finan_opt == "go": #Different choices that go into the different functions, this one goes into the Savings/Goal Trackers.
+                pass
                 break
-            elif pers_finan_opt == "data":
-                print("This is the '2' option, 'Data Manager', if it is working, then great! Don't forget to callback the actual functions here later!")
+            elif pers_finan_opt == "data": #This one goes into the Data Manager.
+                pass
                 break
-            elif pers_finan_opt == "convert":
-                print("This is the '3' option, 'Convert Currency', if it is working, then great! Don't forget to callback the actual functions here later!")
+            elif pers_finan_opt == "convert": #This one goes into the Convert Currency.
+                pass
                 break
-            elif pers_finan_opt == "check":
-                print("This is the '4' option, 'Check Budgeting', if it is working, then great! Don't forget to callback the actual functions here later!")
+            elif pers_finan_opt == "check": #This one goes into the Check Budgetting.
+                pass
                 break
-            elif pers_finan_opt == "exit":
-                print("This is the '5' option, 'Exit', if it is working, then great! Don't forget to callback the actual functions here later!")
+            elif pers_finan_opt == "exit": #As the name implies, exits and quits the program.
+                print("Goodbye, thank you for using your personal financial manager!")
                 break
         except:
-            ValueError
-            print("This doesn't work! Please input a correct input (1, 2, 3, 4, 5)")
+            ValueError #Error management.
+            print("This doesn't work! Please choose a correct input mainly using the arrow keys.")
 
 main()
 
